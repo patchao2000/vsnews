7253eafd1c8240abc0c66ee010705709ef20e17d
Zhao Peng <patchao2000@gmail.com>
2014-05-23 18:48:12 +0800

    login finished

---
+++ vsnews/src/main/java/com/videostar/vsnews/web/MainController.java
@@ -0,0 +1,24 @@
+package com.videostar.vsnews.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页控制器
+ */
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "/main/index";
+    }
+
+    @RequestMapping(value = "/welcome")
+    public String welcome() {
+        return "/main/welcome";
+    }
+
+}
